0e14b1f4cb0d87232629f9fe00d19b65ba6f00d2
jonathan <colladojonathan01@gmail.com>
2026-05-16 11:35:40 -0600

    comentar la clase para mejorar entendimiento

---
+++ clase 02/main.py
@@ -0,0 +1,26 @@
+print ("hola mundo")
+
+# inicializar variables
+nombre:str = "jonathan"
+
+# solicitar al usuario su edad
+edad :int = int(input("¿cual es mi edad?"))
+
+# calcular año de nacimiento
+anno_de_nacimiento:int = 2026 - edad
+
+# imprimir nombre, edad y año de nacimiento
+print("mi nombre es";nombre)
+
+# calcular mayoria de edad 
+mayor_de_edad:boo1 = edad >= 20
+
+#inicializar variables
+soy_yo = nombre =="jonathan" and edad == 22
+no_soy_yo = not(nombre == "jonathan" and edad == 20)
+quizas_soy_yo = nombre == "jonathan" or edad = 20
+
+#incrementos de variables numericas 
+x = 10
+x += 5
+print(x)
